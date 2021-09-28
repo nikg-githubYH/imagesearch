32462ef729233c4eb861fb719cb2bcb5e7d7abb7
Jorgen-son-of-karl <75936756+Jorgen-son-of-karl@users.noreply.github.com>
2021-09-28 10:55:38 +0200

    js + html ändringar
    utökat js filen till att skapa bilder på sidan, små ändringar i html

--- Bildsökmotor/main.js
+++ Bildsökmotor/main.js
@@ -3,16 +3,37 @@ const apiKey = '23566762-6346aa8b21c47a7baaa4f58ee';
 const form = document.querySelector('form');
 form.onsubmit = async event =>{
 	event.preventDefault();
+	//a new search clears all images
+	removeAllChildren(collage);
 	const motive = form.elements.motive.value;
-	const color = document.getElementById("color").selected;
+	let color = document.querySelector("#color");
 
 	const params = new URLSearchParams({
 		key: apiKey,
 		q: motive,
-		colors: color,
+		colors: color.value,
 		per_page: 10
 	})
 
 	 const response = await fetch('https://pixabay.com/api/?' + params.toString());
 	 const json = await response.json();
+
+	 //testing OBS glöm ej att ta bort!
+	 console.log(json);
+	 console.log(collage.firstChild);
+	
+	for(let i = 0; i < json.hits.length; i++){
+		var elem = document.createElement("img");
+		elem.setAttribute("src", json.hits[i].webformatURL);
+		elem.setAttribute("height", "500");
+		elem.setAttribute("width", "500");
+		document.getElementById("collage").appendChild(elem);
+	}
+
+	// a function to clear all images when we initiate a new search
+	function removeAllChildren(parent){
+		while(parent.firstChild){
+			parent.removeChild(parent.firstChild);
+		}
+	}	
 }
